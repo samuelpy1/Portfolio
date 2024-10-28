6c9445455f05f706f9268aa75611ff9e6ed5c5d3
lemos000 <lemosgabriel2016@gmail.com>
2024-10-27 22:51:34 -0300

    feat: building post request method

---
+++ src/app/api/avaliacoes/route.tsx
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+import { TipoAvaliacao } from "@/types";
+
+export async function POST(request:Request) {
+
+    try {
+        //Recuperação da lista de produtos que está em um arquivo .json e colocamos em uma constante. 
+        const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
+
+        //A lista vem no formato de string, para podermos manipular ela, devemos converter em objeto.
+        const avaliacoes:TipoAvaliacao[] = JSON.parse(file);
+
+        //Recuperar o  objeto que chegou no request e tipar ele, para podermos posteriormente adicionar um novo ID e guardar na lista.
+        const avaliacao:TipoAvaliacao = await request.json();
+
+        //Gerando um novo ID para o novo produto.
+        avaliacao.id = avaliacoes[avaliacoes.length - 1].id+1;
+
+        //Adicionamos o novo produto na lista.
+        avaliacoes.push(avaliacao);
+
+        //Vamos converter a lista para string/JSONpara podermos devolver ela no arquivo .json.
+        const newFile = JSON.stringify(avaliacoes);
+
+        //Finalmente podemos utilizar o fs para escrever ou guardar a lista no arquivo e sobrepor as antigas informações.
+        await fs.writeFile(process.cwd() + "/src/data/base.json", newFile);
+        
+        return NextResponse.json(avaliacao,{status:201})
+
+    } catch (error) {
+        console.error("Falha na criação de um novo produto.", error);
+        return NextResponse.json({msg:"Falha na criação!"},{status:500});
+    }
+}
