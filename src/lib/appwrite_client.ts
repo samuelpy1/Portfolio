89a52087368d8b6f55a6e4f513eb63f18daede07
samuelpy1 <samuelppy1@gmail.com>
2024-10-28 21:48:49 -0300

    clean

--- src/lib/appwrite_client.ts
+++
@@ -1,9 +0,0 @@
-import { Account, Client, Databases } from 'appwrite';
-
-export const client = new Client();
-client
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT_URL as string)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
-
-export const account = new Account(client);
-export const databases = new Databases(client);
